75b16b7c169e5d9ad96effb7d1ee95eda0c71898
Jakub Jankowski <mallachari@gmail.com>
2018-06-10 15:43:57 +0800

    [node] Modify logs for Heroku deployment

--- devbuilder-node/routes/app.js
+++ devbuilder-node/routes/app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+   const env = process.env.NODE_ENV || 'none';
+   const uri = process.env.MONGODB_URI || 'none';
+   const port = process.env.PORT || 'none';
    res.status(200).json({
-      message: 'Success'
+      message: 'Success',
+      env: env,
+      uri: uri,
+      port: port
    });
 });
 
